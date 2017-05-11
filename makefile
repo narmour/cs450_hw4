167811ce81ab9220a3e7ac1b899daecc5418306c
narmour <n.armour0@gmail.com>
2017-05-10 20:33:31 -0700

    done

--- makefile
+++ makefile
@@ -1,7 +1,10 @@
+all: p1.x p2.x p3.x
+
+
 p1.x: p1.c
 	gcc -D PROB1 -o p1.x p1.c
 p2.x: p1.c
-	gcc -D -o p2.x p1.c
+	gcc -o p2.x p1.c
 p3.x: p3.c
 	gcc -o p3.x p3.c
 
--- p3.c
+++ p3.c
@@ -8,6 +8,15 @@
 #include <sys/types.h>
 
 
+/*
+ * When this program is run, I read and print out the first character in the file
+ * then i fork and "delete" the file. However, after the delete, I can still read
+ * from the file. This is because the file is not actually deleted until ALL links
+ * to the file are gone. My fopen() creates a link that lets me still read in after
+ * the unlink() call.
+ */
+
+
 int main(int argc, char **argv){
     if(argc < 2){
         puts("need filename");
@@ -25,6 +34,7 @@ int main(int argc, char **argv){
     //forks a child process that calls unlink() to delete the file
     if(fork() ==0){
         unlink(argv[1]);
+        return 0;
     }
     wait(NULL);
 
@@ -36,6 +46,8 @@ int main(int argc, char **argv){
 
         printf("after delete, read in %c\n",d);
     }
+
+    return 0;
 
 
 
